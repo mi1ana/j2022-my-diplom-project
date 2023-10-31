d48d482516a46c9c634564aff50c4f90ba22ab48
VolkovVyacheslav <volchara.only@gmail.com>
2023-10-31 11:17:42 +0300

    init

---
+++ src/main/java/com/mi1ana/j2022mydiplomproject/J2022MyDiplomProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mi1ana.j2022mydiplomproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J2022MyDiplomProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J2022MyDiplomProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mi1ana/j2022mydiplomproject/J2022MyDiplomProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mi1ana.j2022mydiplomproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J2022MyDiplomProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
